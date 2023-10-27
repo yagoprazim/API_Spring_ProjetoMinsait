26c97ea211503f66d953e2063c51d0ded5fef5ba
yagoprazim <prazimyago@gmail.com>
2023-10-27 17:28:32 -0300

    criação da model Pessoa, seus atributos, validações básicas e anotações

---
+++ src/main/java/br/com/YagoPrazim/ApiControleContatos/models/Pessoa.java
@@ -0,0 +1,29 @@
+package br.com.YagoPrazim.ApiControleContatos.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pessoas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Pessoa {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String endereco;
+
+    private String cep;
+
+    private String cidade;
+
+    private String uf;
+}
