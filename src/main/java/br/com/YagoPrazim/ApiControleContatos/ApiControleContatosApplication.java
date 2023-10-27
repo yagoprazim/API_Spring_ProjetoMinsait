94ebae13cc990a6433301f1953e9659637177ad7
yagoprazim <prazimyago@gmail.com>
2023-10-27 16:55:17 -0300

    configuração básica do ambiente e criação da estrutura inicial

---
+++ src/main/java/br/com/YagoPrazim/ApiControleContatos/ApiControleContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.YagoPrazim.ApiControleContatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/YagoPrazim/ApiControleContatos/ApiControleContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.YagoPrazim.ApiControleContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
